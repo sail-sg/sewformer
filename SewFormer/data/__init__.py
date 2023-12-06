aafa2420ee1e65c19c64d35fba6da8f87c7d86cf
xyyeah <liuljyeah@hotmail.com>
2023-12-06 17:57:29 +0800

    delete SewFormer

--- SewFormer/data/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Custom datasets & dataset wrapper (split & dataset manager) """
-
-
-from .dataset import GarmentDetrDataset
-from .wrapper import RealisticDatasetDetrWrapper
-from .pattern_converter import NNSewingPattern, InvalidPatternDefError, EmptyPanelError
--- SewFormer/data/human_body_prior/__init__.py
+++
@@ -1,2 +0,0 @@
-from data.human_body_prior.body_model import BodyModel
-from data.human_body_prior.lbs import lbs
